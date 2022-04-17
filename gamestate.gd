f75ad007e037f97a4851e0fdba1db30a132504a6
InfuriatedBrute <bobitron74@gmail.com>
2022-04-17 14:04:34 -0400

    more prints

--- gamestate.gd
+++ gamestate.gd
@@ -89,7 +89,7 @@ func load_world():
 func host_game(new_player_name):
 	player_name = new_player_name
 	peer = ENetMultiplayerPeer.new()
-	peer.create_server(DEFAULT_PORT, MAX_PEERS)
+	print("create server return: ", peer.create_server(DEFAULT_PORT, MAX_PEERS))
 	multiplayer.set_multiplayer_peer(peer)
 	print("player_name: ", player_name)
 	print("peer: ", peer)
